9512829a8c3477840303dd02ba564c415fb9ef74
Tony Jinks <tony.jinks@gmail.com>
2024-02-09 11:47:46 +0000

    Ongoing dev

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ChessLib",
     platforms: [
-        .macOS(.v14), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v12), .iOS(.v13), .tvOS(.v13)
         ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
